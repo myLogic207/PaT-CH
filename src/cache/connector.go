bc85cb3151018a19c9c481b7877347c246e1bfc0
myLogic207 <lijon.fogel@gmail.com>
2023-02-13 09:09:28 +0100

    remove fallback

--- src/cache/connector.go
+++ src/cache/connector.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"context"
 	"errors"
+	"log"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -14,79 +15,54 @@ var ctx = context.Background()
 // 	Set(key string, value string) error
 // }
 
-type Connector struct {
-	RedisConnector
-	FallBackConnector
-}
-
-type FallBackConnector struct {
-	Cache map[string]string
-}
-
 type RedisConnector struct {
+	db     int
 	active bool
 	Cache  *redis.Client
 }
 
-func (c *Connector) Get(key string) (string, error) {
-	// if Redis get from Redis
-	if c.RedisConnector.active {
-		return c.RedisConnector.Cache.Get(ctx, key).Result()
-	}
-	// Get from fallback
-	if value, ok := c.FallBackConnector.Cache[key]; ok {
-		return value, nil
-	}
-	return "", errors.New("key not found")
-}
-
-func (c *Connector) Set(key string, value string) error {
-	if c.RedisConnector.active {
-		return c.RedisConnector.Cache.Set(ctx, key, value, 0).Err()
-	}
-	c.FallBackConnector.Cache[key] = value
-	return nil
-}
-
-func NewConnector(url string, db int) *Connector {
-	return &Connector{
-		RedisConnector:    NewRedisConnector(url, db),
-		FallBackConnector: NewFallBackConnector(),
-	}
-}
-
-func NewFallBackConnector() FallBackConnector {
-	return FallBackConnector{
-		Cache: make(map[string]string),
-	}
-}
-
-func NewRedisConnector(url string, db int) RedisConnector {
+func NewConnector(url string, db int) *RedisConnector {
 	options, err := redis.ParseURL(url)
 	if err != nil {
 		panic(err)
 	}
 	client := redis.NewClient(options)
-	return RedisConnector{
+	return &RedisConnector{
+		db:     db,
 		Cache:  client,
 		active: false,
 	}
 }
 
-func (c *Connector) Connect() error {
-	if err := c.RedisConnector.Cache.Ping(ctx).Err(); err != nil {
-		println(err.Error())
-		println("failed to connect to Redis cache, using in-memory fallback")
-		c.RedisConnector.active = false
+func (c *RedisConnector) Connect() error {
+	if err := c.Cache.Ping(ctx).Err(); err != nil {
+		log.Println(err)
+		c.active = false
 		return nil
 	}
-	c.RedisConnector.active = true
+	c.active = true
+	log.Println("connected to Redis cache")
 	return nil
 }
 
-func (c *Connector) Close() error {
-	if c.RedisConnector.active {
-		return c.RedisConnector.Cache.Close()
+func (c *RedisConnector) Close() error {
+	if c.active {
+		return c.Cache.Close()
 	}
-	return nil
+	return errors.New("redis already closed")
+}
+
+func (c *RedisConnector) Get(key string) (string, error) {
+	// if Redis get from Redis
+	if c.active {
+		return c.Cache.Get(ctx, key).Result()
+	}
+	return "", errors.New("redis not active")
+}
+
+func (c *RedisConnector) Set(key string, value string) error {
+	if c.active {
+		return c.Cache.Set(ctx, key, value, 0).Err()
+	}
+	return errors.New("redis not active")
 }
