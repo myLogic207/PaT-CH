c99e3aaf95fda7ff05fa83aef9b94363f4227c6a
myLogic207 <lijon.fogel@gmail.com>
2023-02-17 00:28:14 +0100

    Add formatter and clean routes

--- src/api/middleware.go
+++ src/api/middleware.go
@@ -1,32 +1,68 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
+type SessionLog struct {
+	TimeStamp    string        `json:"time_stamp"`
+	ClientIP     string        `json:"client_ip"`
+	Method       string        `json:"method"`
+	Path         string        `json:"path"`
+	Proto        string        `json:"proto"`
+	StatusCode   int           `json:"status_code"`
+	Latency      time.Duration `json:"latency"`
+	UserAgent    string        `json:"user_agent"`
+	ErrorMessage string        `json:"error_message"`
+}
+
+var apiSkipPaths = []string{"/api/v1/health"}
+
+const apiConnectURL = "/api/v1/auth/connect"
+
 func NewRouter(sessionCtl *SessionControl, cache sessions.Store) *gin.Engine {
 	router := gin.New()
-	router.Use(gin.Logger())
+
+	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+		Formatter: apiLogFormatter,
+		Output:    os.Stdout,
+		SkipPaths: apiSkipPaths,
+	}))
+
 	router.Use(gin.Recovery())
 	router.Use(sessions.Sessions("mysession", cache))
 
-	router.GET("/ping", routePing)
+	if os.Getenv("ENVIROMENT") == "development" {
+		router.Use(gin.Logger())
+		router.GET("/ping", routePing)
+	}
 
-	api := router.Group("/api")
-	apiV1 := api.Group("/v1")
-
-	apiV1.GET("/health", routeHealth)
-	apiV1.POST("/connect", sessionCtl.Connect)
-	apiV1.POST("/disconnect", sessionCtl.Disonnect)
-
-	apiV1Auth := apiV1.Group("/auth")
-	apiV1Auth.Use(RoutePass())
-	apiV1Auth.GET("/session", GetID)
+	addApiRoutes(router.Group("/api"), sessionCtl)
 
 	return router
+}
+
+func addApiRoutes(api *gin.RouterGroup, sessionCtl *SessionControl) {
+	addV1Routes(api.Group("/v1"), sessionCtl)
+}
+
+func addV1Routes(v1 *gin.RouterGroup, sessionCtl *SessionControl) {
+	v1.GET("/health", routeHealth)
+	addAuthRoutes(v1.Group("/auth"), sessionCtl)
+}
+
+func addAuthRoutes(auth *gin.RouterGroup, sessionCtl *SessionControl) {
+	auth.Use(RoutePass)
+	auth.POST("/connect", sessionCtl.Connect)
+	auth.POST("/disconnect", sessionCtl.Disonnect)
+	auth.GET("/session", GetID)
 }
 
 // routes
@@ -40,28 +76,48 @@ func routePing(c *gin.Context) {
 // Api routes (v1)
 
 func routeHealth(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-	})
+	// Return a simple object to indicate that the service is up.
+	c.Status(http.StatusOK)
 }
 
-func RoutePass() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		sessionID := session.Get("id")
-		if sessionID == nil {
-			c.JSON(http.StatusForbidden, gin.H{
-				"message": "unauthorized",
-			})
-			c.Abort()
-		}
+func RoutePass(c *gin.Context) {
+	session := sessions.Default(c)
+	if session.Get("id") == nil && c.Request.URL.Path != apiConnectURL {
+		c.JSON(http.StatusForbidden, gin.H{
+			"message": "unauthorized",
+		})
+		c.Abort()
 	}
 }
 
+// GetID returns the id of the current user.
 func GetID(c *gin.Context) {
 	session := sessions.Default(c)
 	c.JSON(http.StatusOK, gin.H{
-		// This is "complicated" to test session exists (so we need to check beforehand as well?)
-		"message": session.Get("id").(string),
+		"id": session.Get("id").(string),
 	})
+}
+
+func apiLogFormatter(params gin.LogFormatterParams) string {
+	outLog := createLog(params)
+	out, err := json.Marshal(outLog)
+	if err != nil {
+		return fmt.Sprintf("LOG ERRROR - REPORT IMMEDIATELY\n-----\n%s\n-----\n", err.Error())
+	}
+	return string(out)
+}
+
+func createLog(params gin.LogFormatterParams) *SessionLog {
+	outLog := &SessionLog{
+		TimeStamp:    params.TimeStamp.Format("02/Jan/2006:15:04:05 -0700"),
+		ClientIP:     params.ClientIP,
+		Method:       params.Method,
+		Path:         params.Path,
+		Proto:        params.Request.Proto,
+		StatusCode:   params.StatusCode,
+		Latency:      params.Latency,
+		UserAgent:    params.Request.UserAgent(),
+		ErrorMessage: params.ErrorMessage,
+	}
+	return outLog
 }
