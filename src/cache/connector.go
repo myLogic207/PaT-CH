2748288c42c80c3301ea5df2b2cd9d21591030d9
myLogic207 <lijon.fogel@gmail.com>
2023-02-18 00:09:46 +0100

    Add config parser

--- src/cache/connector.go
+++ src/cache/connector.go
@@ -5,13 +5,17 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"strconv"
 
+	"github.com/mylogic207/PaT-CH/system"
 	"github.com/redis/go-redis/v9"
 )
 
 var ctx = context.Background()
 
-type Config struct {
+var logger = log.New(log.Writer(), "cache: ", log.Flags())
+
+type RedisConfig struct {
 	Host       string
 	Port       uint16
 	Password   string
@@ -22,8 +26,88 @@ type Config struct {
 	// Addr     func() string
 }
 
-func (c Config) Addr() string {
+func (c RedisConfig) Addr() string {
 	return c.Host + ":" + fmt.Sprint(c.Port)
+}
+
+func ParseConf(config *system.ConfigMap) (*RedisConfig, error) {
+	var err error
+	outConf := &RedisConfig{
+		Host:       "localhost",
+		Port:       6379,
+		Password:   "",
+		DB:         1,
+		Idle:       10,
+		MaxActive:  100,
+		TimeoutSec: 60,
+	}
+	if val, ok := config.Get("host"); ok {
+		outConf.Host = val
+	} else {
+		logger.Println("could not determine redis host, using localhost")
+	}
+	if val, ok := config.Get("port"); ok {
+		p, err := strconv.ParseUint(val, 10, 16)
+		if err != nil {
+			logger.Println("could not parse redis port, using default")
+		} else {
+			outConf.Port = uint16(p)
+		}
+	} else {
+		logger.Println("could not determine redis port, using default")
+	}
+
+	if val, ok := config.Get("password"); ok {
+		outConf.Password = val
+	} else {
+		logger.Println("could not determine redis password, using none")
+	}
+
+	if val, ok := config.Get("db"); ok {
+		p, err := strconv.ParseInt(val, 10, 32)
+		if err != nil {
+			logger.Println("could not parse redis db, using default")
+		} else {
+			outConf.DB = int(p)
+		}
+	} else {
+		logger.Println("could not determine redis db, using default")
+	}
+
+	if val, ok := config.Get("idle"); ok {
+		p, err := strconv.ParseInt(val, 10, 32)
+		if err != nil {
+			logger.Println("could not parse redis idle, using default")
+		} else {
+			outConf.Idle = int(p)
+		}
+	} else {
+		logger.Println("could not determine redis idle, using default")
+	}
+
+	if val, ok := config.Get("max_active"); ok {
+		p, err := strconv.ParseInt(val, 10, 32)
+		if err != nil {
+			logger.Println("could not parse redis max_active, using default")
+		} else {
+			outConf.MaxActive = int(p)
+		}
+	} else {
+		logger.Println("could not determine redis max_active, using default")
+	}
+
+	if val, ok := config.Get("timeout_sec"); ok {
+		p, err := strconv.ParseInt(val, 10, 32)
+		if err != nil {
+			logger.Println("could not parse redis timeout_sec, using default")
+		} else {
+			outConf.TimeoutSec = int(p)
+		}
+	} else {
+		logger.Println("could not determine redis timeout_sec, using default")
+	}
+
+	return outConf, err
 }
 
 type RedisConnector struct {
@@ -31,7 +115,15 @@ type RedisConnector struct {
 	Store  *redis.Client
 }
 
-func NewConnector(config *Config) *RedisConnector {
+func NewConnectorPreConf(config *system.ConfigMap) *RedisConnector {
+	redisConfig, err := ParseConf(config)
+	if err != nil {
+		logger.Println("could not parse redis config, using default(s)")
+	}
+	return NewConnector(redisConfig)
+}
+
+func NewConnector(config *RedisConfig) *RedisConnector {
 	connection := redis.NewClient(&redis.Options{
 		Addr:     config.Addr(),
 		Password: config.Password,
@@ -45,12 +137,12 @@ func NewConnector(config *Config) *RedisConnector {
 
 func (c *RedisConnector) Connect() error {
 	if err := c.Store.Ping(ctx).Err(); err != nil {
-		log.Println(err)
+		logger.Println(err)
 		c.Active = false
 		return nil
 	}
 	c.Active = true
-	log.Println("connected to Redis cache")
+	logger.Println("connected to Redis cache")
 	return nil
 }
 
