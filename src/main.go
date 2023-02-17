48b3f28365bda66f2f44642bfb707444fef54f68
myLogic207 <lijon.fogel@gmail.com>
2023-02-18 00:10:30 +0100

    update main

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/mylogic207/PaT-CH/api"
+	"github.com/mylogic207/PaT-CH/cache"
 	"github.com/mylogic207/PaT-CH/system"
 )
 
@@ -12,17 +13,10 @@ func main() {
 	config := system.LoadConfig("PATCH")
 	config.Print()
 
-	// sessionCache := cache.NewConnector(config.redisConfig)
-	// err := sessionCache.Connect()
-	// defer sessionCache.Close()
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-
 	var apiConfig *api.ApiConfig
 	if val, ok := config.Get("api"); ok {
-		if cMap, ok := val.(system.ConfigMap); ok {
-			conf, err := api.ParseConf(&cMap)
+		if cMap, ok := val.(*system.ConfigMap); ok {
+			conf, err := api.ParseConf(cMap)
 			if err != nil {
 				println("Error getting API conf, using default values")
 				apiConfig = &api.ApiConfig{
@@ -37,9 +31,38 @@ func main() {
 	} else {
 		println("Error getting API conf, using default values")
 	}
-	server := api.NewServerPreConf(apiConfig)
+
+	var redisConf *cache.RedisConfig
+	if val, ok := config.Get("redis"); ok {
+		if cMap, ok := val.(*system.ConfigMap); ok {
+			conf, err := cache.ParseConf(cMap)
+			if err != nil {
+				println("Error getting redis conf, using default values")
+				redisConf = &cache.RedisConfig{
+					Host:       "localhost",
+					Port:       6379,
+					Password:   "",
+					DB:         1,
+					Idle:       10,
+					MaxActive:  100,
+					TimeoutSec: 60,
+				}
+			} else {
+				redisConf = conf
+			}
+		}
+	} else {
+		println("Error getting redis conf, using default values")
+	}
+
+	server := api.NewServer(apiConfig)
+	cache := cache.NewConnector(redisConf)
+
 	server.Start()
-	// defer server.Stop()
+	defer server.Stop()
+
+	cache.Connect()
+	defer cache.Close()
 	time.Sleep(10000 * time.Second)
 	// print("done")
 }
