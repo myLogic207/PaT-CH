c045f45537b6946c20e819126dc35e88ebacb8a2
myLogic207 <lijon.fogel@gmail.com>
2023-03-03 20:14:32 +0100

    Add api userdb integration

--- src/api/sessions.go
+++ src/api/sessions.go
@@ -1,23 +1,32 @@
 package api
 
 import (
+	"fmt"
 	"math/rand"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/mylogic207/PaT-CH/system"
 )
 
 const id_bytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
+var (
+	ErrRegister = fmt.Errorf("error registering user")
+)
+
 type SessionControl struct {
 	key_len  int
 	sessions map[string]sessions.Session
+	db       UserTable
 }
 
-func NewSessionControl() *SessionControl {
+func NewSessionControl(db UserTable) *SessionControl {
 	return &SessionControl{
-		key_len: 16,
+		key_len:  16,
+		db:       db,
+		sessions: make(map[string]sessions.Session),
 	}
 }
 
@@ -30,6 +39,7 @@ func (s *SessionControl) idExists(id string) bool {
 	return false
 }
 
+// todo: make this more secure
 func (s *SessionControl) getNewId() string {
 	var id string
 	for ok := true; ok; ok = s.idExists(id) {
@@ -44,8 +54,48 @@ func (s *SessionControl) getNewId() string {
 	return id
 }
 
-func (s SessionControl) Connect(c *gin.Context) {
+func (s *SessionControl) SetDB(db UserTable) {
+	s.db = db
+}
+
+func (s *SessionControl) register(c *gin.Context) {
+	var raw rawUser
+	if err := c.BindJSON(&raw); err != nil {
+		logger.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": ErrRegister})
+		return
+	}
+	user, err := s.db.Create(c, raw.Username, "", raw.Password)
+	if err != nil {
+		logger.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": ErrRegister})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "registered",
+		"user":    user,
+	})
+}
+
+type rawUser struct {
+	Username string `json:"username"`
+	// Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (s *SessionControl) Connect(c *gin.Context) {
+	var raw rawUser
+	if err := c.ShouldBindJSON(&raw); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := s.db.Authenticate(c, raw.Username, raw.Password)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	session := sessions.Default(c)
+	session.Set("user", user)
 	id := s.getNewId()
 	session.Set("id", id)
 	session.Save()
@@ -55,11 +105,77 @@ func (s SessionControl) Connect(c *gin.Context) {
 	})
 }
 
-func (s SessionControl) Disonnect(c *gin.Context) {
+func (s *SessionControl) Disonnect(c *gin.Context) {
 	session := sessions.Default(c)
 	session.Clear()
 	session.Save()
 	c.JSON(http.StatusOK, gin.H{
 		"message": "disconnected",
+	})
+}
+
+func (s *SessionControl) Status(c *gin.Context) {
+	session := sessions.Default(c)
+	if session.Get("id") == nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"message": "disconnected",
+		})
+		c.Abort()
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "connected",
+	})
+}
+
+func (s *SessionControl) GetUser(c *gin.Context) {
+	session := sessions.Default(c)
+	if session.Get("id") == nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"message": "disconnected",
+		})
+		c.Abort()
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"user": session.Get("user"),
+	})
+}
+
+func (s *SessionControl) UpdateUser(c *gin.Context) {
+	session := sessions.Default(c)
+	if session.Get("id") == nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"message": "disconnected",
+		})
+		return
+	}
+	user := session.Get("user").(system.User)
+	updated, err := s.db.Update(c, &user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "updated",
+		"user":    updated,
+	})
+}
+
+func (s *SessionControl) DeleteUser(c *gin.Context) {
+	session := sessions.Default(c)
+	if session.Get("id") == nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"message": "disconnected",
+		})
+		return
+	}
+	user := session.Get("user").(system.User)
+	if err := s.db.DeleteByName(c, user.Name); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "deleted",
 	})
 }
