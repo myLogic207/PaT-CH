90b111f3023fbdbdba606a7cf23f5a9869ec0778
myLogic207 <lijon.fogel@gmail.com>
2023-03-03 17:59:21 +0100

    Add(user): More GET and fixes

--- src/storage/data/userdb.go
+++ src/storage/data/userdb.go
@@ -2,14 +2,21 @@ package data
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
+)
+
+var (
+	ErrNoUser = fmt.Errorf("no user found")
 )
 
 type User struct {
-	id        int
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	CreatedAt string `json:"created_at"`
+	id        int64
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	updatedAt time.Time
 	password  string
 }
 
@@ -24,40 +31,90 @@ func (u *User) SetPassword(password string) {
 }
 
 type UserDB struct {
-	p *DataBase
+	p         *DataBase
+	usertable string
+}
+
+func NewUserDB(p *DataBase, usertable string) *UserDB {
+	return &UserDB{p, usertable}
+}
+
+func (udb *UserDB) SetTableName(usertable string) {
+	udb.usertable = usertable
 }
 
 func (udb *UserDB) Create(ctx context.Context, name string, email string, password string) error {
 	// TODO: Hash password
 	passwordHash := password
-	return udb.p.Insert(ctx, "users", []FieldName{"name", "email", "password"}, [][]interface{}{{name, email, passwordHash}})
+	now := time.Now().UTC()
+	return udb.p.Insert(ctx, udb.usertable, []FieldName{"name", "email", "password", "created_at", "updated_at"}, [][]interface{}{{name, email, passwordHash, now, now}})
 }
 
-func (udb *UserDB) GetById(ctx context.Context, id int) (*User, error) {
-	val, err := udb.p.Select(ctx, "users", []FieldName{"id", "name", "email", "created_at"}, NewWhereMap(map[FieldName]interface{}{"id": id}), "LIMIT 1")
+func (udb *UserDB) GetAll(ctx context.Context) ([]*User, error) {
+	val, err := udb.p.Select(ctx, udb.usertable, []FieldName{"*"}, nil, "")
+	if err != nil {
+		return nil, err
+	}
+	users := make([]*User, len(val))
+	for i, v := range val {
+		users[i] = &User{
+			id:        v["id"].(int64),
+			Name:      v["name"].(string),
+			Email:     v["email"].(string),
+			CreatedAt: v["created_at"].(time.Time),
+		}
+	}
+	return users, nil
+}
+
+func (udb *UserDB) GetByName(ctx context.Context, name string) (*User, error) {
+	val, err := udb.p.Select(ctx, udb.usertable, []FieldName{"*"}, NewWhereMap(map[FieldName]interface{}{"name": name}), "LIMIT 1")
 	if err != nil {
 		return nil, err
 	}
 	if len(val) == 0 {
-		return nil, nil
+		return nil, ErrNoUser
 	}
 	return &User{
-		id:        val[0]["id"].(int),
+		id:        val[0]["id"].(int64),
 		Name:      val[0]["name"].(string),
 		Email:     val[0]["email"].(string),
-		CreatedAt: val[0]["created_at"].(string),
+		CreatedAt: val[0]["created_at"].(time.Time),
+		updatedAt: val[0]["updated_at"].(time.Time),
+		password:  val[0]["password"].(string),
+	}, nil
+}
+
+func (udb *UserDB) GetById(ctx context.Context, id int64) (*User, error) {
+	val, err := udb.p.Select(ctx, udb.usertable, []FieldName{"*"}, NewWhereMap(map[FieldName]interface{}{"id": id}), "LIMIT 1")
+	if err != nil {
+		return nil, err
+	}
+	if len(val) == 0 {
+		return nil, ErrNoUser
+	}
+	return &User{
+		id:        val[0]["id"].(int64),
+		Name:      val[0]["name"].(string),
+		Email:     val[0]["email"].(string),
+		CreatedAt: val[0]["created_at"].(time.Time),
+		updatedAt: val[0]["updated_at"].(time.Time),
+		password:  val[0]["password"].(string),
 	}, nil
 }
 
 func (udb *UserDB) Update(ctx context.Context, user *User) error {
+	timestamp := fmt.Sprint(time.Now().UTC())
+	timestamp = timestamp[:len(timestamp)-9]
 	usermap := map[FieldName]DBValue{
-		"name":     user.Name,
-		"email":    user.Email,
-		"password": user.Password(),
+		"name":       user.Name,
+		"email":      user.Email,
+		"password":   user.Password(),
+		"updated_at": timestamp,
 	}
-	return udb.p.Update(ctx, "users", usermap, NewWhereMap(map[FieldName]interface{}{"id": user.id}))
+	return udb.p.Update(ctx, udb.usertable, usermap, NewWhereMap(map[FieldName]interface{}{"id": user.id}))
 }
 
-func (udb *UserDB) DeleteById(ctx context.Context, id int) error {
-	return udb.p.Delete(ctx, "users", NewWhereMap(map[FieldName]interface{}{"id": id}))
+func (udb *UserDB) DeleteById(ctx context.Context, id int64) error {
+	return udb.p.Delete(ctx, udb.usertable, NewWhereMap(map[FieldName]interface{}{"id": id}))
 }
